ba1b4f005939e979c4033bc6fdfd72f118ce1a4e
katemona <k.monahan@mothermcauley.org>
2018-08-06 20:24:37 +0000

    automatically backed up by learn

---
+++ interpolation-practice.rb
@@ -0,0 +1,12 @@
+def my_fav_things
+
+fav_animal = "monkeys"
+fav_place = "Chicago"
+fav_artist = "Picasso"
+
+  puts "I love #{fav_animal}"
+  puts "I love #{fav_place}"
+  puts "I love #{fav_artist}"
+end
+
+my_fav_things
